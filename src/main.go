f5623b71d22fe158691fc59fd0593d54b18aa51f
Luis Gonzalez <lgonzalez@talana.com>
2022-09-16 18:17:36 -0500

    fmt funciones

--- src/main.go
+++ src/main.go
@@ -4,6 +4,28 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Hello, World!")
+	// declare a variable
+	helloMessage := "Hello"
+	worldMessage := "World!"
+
+	// print the variable
+
+	// println	- print with a new line
+	fmt.Println(helloMessage, worldMessage)
+	fmt.Println(helloMessage, worldMessage)
+
+	// printf	- print with a format
+	nombre := "Juan"
+	edad := 20
+	fmt.Printf("Hola, mi nombre es %s y tengo %d añosa\n", nombre, edad)
+	fmt.Printf("Hola, mi nombre es %v y tengo %v añosa\n", nombre, edad)
+
+	// Sprintf	- print with a format and return a string
+	message := fmt.Sprintf("Hola, mi nombre es %s y tengo %d añosa", nombre, edad)
+	fmt.Println(message)
+
+	//  tipos de datos
+	fmt.Printf("helloMessage: %T\n", helloMessage)
+	fmt.Printf("edad: %T\n", edad)
 
 }
