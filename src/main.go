a4ad5535a78e499a58b16d493501c076fa7b70d6
Luis Gonzalez <lgonzalez@talana.com>
2022-09-16 19:05:49 -0500

    Condicional if

--- src/main.go
+++ src/main.go
@@ -1,64 +1,52 @@
 package main
 
+import (
+	"log"
+	"strconv"
+)
+
 func main() {
 
-	// For loop with single condition
-	for i := 0; i <= 10; i++ {
-		println(i)
+	// condicional if
+
+	valor := 20
+
+	if valor == 10 {
+		println("El valor es 10")
 	}
 
-	// For loop with multiple conditions
-	for i, j := 0, 10; i <= 10 && j >= 0; i, j = i+1, j-1 {
-		println(i, j)
+	if valor := 10; valor == 10 {
+		println("El valor es 10")
 	}
 
-	// For while loop
-	i := 0
-	for i <= 10 {
-		println(i)
-		i++
+	// condicional switch
+
+	switch valor {
+	case 10:
+		println("El valor es 10")
+	case 20:
+		println("El valor es 20")
+	default:
+		println("El valor es otro")
 	}
 
-	// For loop with break
-	for i := 0; i <= 10; i++ {
-		if i == 5 {
-			break
-		}
-		println(i)
+	// condicional  with  and	& or
+
+	if valor > 10 && valor < 30 {
+		println("El valor es mayor a 10 y menor a 30")
 	}
 
-	// For loop with continue
-	for i := 0; i <= 10; i++ {
-		if i == 5 {
-			continue
-		}
-		println(i)
-
+	if valor > 10 || valor < 30 {
+		println("El valor es mayor a 10 o menor a 30")
 	}
 
-	// For loop with labels
-Loop:
-	for i := 0; i <= 10; i++ {
-		for j := 0; j <= 10; j++ {
-			if i == 5 && j == 5 {
-				break Loop
-			}
-			println(i, j)
-		}
+	// convertir texto a numero
+	value, err := strconv.Atoi("213123")
+	if err != nil {
+		println("Error al convertir")
+		log.Fatal(err)
+	} else {
+		println(value)
 	}
 
-	// For loop with range
-	for i, j := range []int{1, 2, 3, 4, 5} {
-		println(i, j)
-	}
-
-	// For loop with range and _
-	for _, j := range []int{1, 2, 3, 4, 5} {
-		println(j)
-	}
-
-	// for forever loop
-	for {
-		println("forever")
-	}
 }
